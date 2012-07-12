9b3a09c2d84988a4adfa06258789814f58705a79
Daniel Shmuglin <daniels@wix.com>
2012-07-13 01:42:41 +0300

    added sample JSON generator + files

--- NodeClient/WebSocketServer/src/main/webapp/js/node.js
+++ NodeClient/WebSocketServer/src/main/webapp/js/node.js
@@ -4,7 +4,7 @@
  * Date: 7/12/12
  */
 
-function NodeCtrl($scope, $filter)
+function NodeCtrl($scope, $filter, $http)
 {
     $scope.worker = {};
 
@@ -12,13 +12,17 @@ function NodeCtrl($scope, $filter)
         Idle : {"label" : "Connected, idle.", "cssClass" : "label-info"},
         Running : {"label" : "Connected, executing task.", "cssClass" : "label-warning"},
         Disconnected : {"label" : "Disconnected.", "cssClass" : ""}
-    }
+    };
 
     $scope.status = $scope.StatusLabel.Disconnected;
 
     $scope.general = {};
-    $scope.general.server = "111.222.333.444:1234";
+    $scope.general.server = window.server;
     $scope.general.account = "AlexanderVa (alexanderva@gmail.com)";
+
+    $scope.socket = {};
+
+    $scope.nodeId = {};
 
     $scope.job = {
         "name" : "Job name here...",
@@ -47,19 +51,67 @@ function NodeCtrl($scope, $filter)
                 }
             });
         };
-    }
+    };
 
     $scope.connect = function() {
-        //TODO: connect
-        $scope.status = $scope.StatusLabel.Idle;
-        $scope.log.println("Connected to " + $scope.general.server);
-    }
+        $scope.log.println("Trying to connect to server " + $scope.general.server);
+
+        var userInfo = {
+            "browser" : $.browser
+        };
+
+        $scope.socket = io.connect($scope.general.server);
+
+        $scope.socket.on('connect', function() {
+            $scope.$apply(function() {
+                $scope.status = $scope.StatusLabel.Idle;
+                $scope.log.println("Connected to " + $scope.general.server);
+            });
+        });
+
+        $scope.socket.on('job', function(job) {
+            $scope.$apply(function() {
+                $scope.job.name = job.details.name;
+                $scope.job.phase = job.details.phase;
+                //TODO: data & handler
+                $scope.run();
+            });
+        });
+
+        $scope.socket.on('register', function(msg) {
+            $scope.$apply(function() {
+                $scope.nodeId = msg.nodeId;
+                $scope.log.println("Client node id [" + msg.nodeId + "]");
+                $scope.socket.emit('userInfo', $filter('json')(userInfo));
+            });
+        });
+    };
 
     $scope.disconnect = function() {
         //TODO: disconnect
+        if ($scope.socket) {
+            $scope.socket.disconnect();
+            $scope.socket = {};
+        }
+
         $scope.status = $scope.StatusLabel.Disconnected;
         $scope.log.println("Disconnected from " + $scope.general.server);
-    }
+    };
+
+    $scope.download = function(fileName) {
+        $http.jsonp('http://shmuglindaniel.blob.core.windows.net/mapreducejs/' + fileName).
+                success(function(data, status, headers, config) {
+                    // this callback will be called asynchronously
+                    // when the response is available
+                    window.alert("success " + status);
+                }).
+                error(function(data, status, headers, config) {
+                    // called asynchronously if an error occurs
+                    // or server returns response with status
+                    // code outside of the <200, 400) range
+                    window.alert("error " + status);
+                });
+    };
 
     $scope.log = {
         data : "",
@@ -71,3 +123,4 @@ function NodeCtrl($scope, $filter)
         }
     };
 }
+
